8007b6a5a646952ee38a805286bc8e41b8ea995f
wldnjs7064 <wldnjs7064@naver.com>
2024-09-29 17:36:19 +0900

    Fix: 테스트 어노테이션 삭제

--- src/test/java/com/example/mydata/MydataApplicationTests.java
+++ src/test/java/com/example/mydata/MydataApplicationTests.java
@@ -3,7 +3,6 @@ package com.example.mydata;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class MydataApplicationTests {
 
 	@Test
