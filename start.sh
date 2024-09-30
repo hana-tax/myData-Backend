b6c29150340057cff9517036a6653635dda74b8c
wldnjs7064 <wldnjs7064@naver.com>
2024-09-30 16:15:10 +0900

    Fix: start.sh 수정

--- start.sh
+++ start.sh
@@ -14,7 +14,7 @@ TIME_NOW=$(date +%c)
 
 # 이전 실행 중인 애플리케이션 종료
 echo "$TIME_NOW > 기존 실행 중인 애플리케이션 종료 시도" >> $DEPLOY_LOG
-CURRENT_PID=$(pgrep -f $JAR_FILE)
+CURRENT_PID=$(pgrep -fl $JAR_FILE | grep java | awk '{print $1}')
 
 if [ -z "$CURRENT_PID" ]; then
     echo "$TIME_NOW > 현재 실행 중인 애플리케이션이 없습니다." >> $DEPLOY_LOG
@@ -25,13 +25,19 @@ else
 fi
 
 # 새로운 빌드 파일 복사
-echo "$TIME_NOW > 새로운 빌드 파일 복사" >> $DEPLOY_LOG
-cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+echo "$TIME_NOW > 새로운 빌드 파일 복사 시도" >> $DEPLOY_LOG
+if [ -f "$PROJECT_ROOT/build/libs/mydata-0.0.1-SNAPSHOT.jar" ]; then
+    echo "$TIME_NOW > 빌드 파일이 발견되었습니다." >> $DEPLOY_LOG
+    cp $PROJECT_ROOT/build/libs/mydata-0.0.1-SNAPSHOT.jar $JAR_FILE
+else
+    echo "$TIME_NOW > 빌드 파일이 없습니다. 배포를 중지합니다." >> $DEPLOY_LOG
+    exit 1
+fi
 
 # 새로운 애플리케이션 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+nohup java -jar $JAR_FILE --spring.profiles.active=prod > $APP_LOG 2> $ERROR_LOG &
 
 # 실행된 애플리케이션 PID 확인
-CURRENT_PID=$(pgrep -f $JAR_FILE)
+CURRENT_PID=$(pgrep -fl $JAR_FILE | grep java | awk '{print $1}')
 echo "$TIME_NOW > 새로 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
